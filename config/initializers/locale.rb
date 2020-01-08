19b411d76f0e64007e062fdf5339f6fc6d22bb24
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-01-08 11:53:51 -0300

    Add :cn to the list of allowed locales.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@ require 'i18n/backend/fallbacks'
 
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.fallbacks.map(cn: :"zh-CN")
-I18n.available_locales = :en
+I18n.available_locales = %i[en cn]
